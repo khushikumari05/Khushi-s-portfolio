0e4d246fe567197205d1ba3bfc53cba46dd97cba
khushikumari05 <khushigupta79kk@gmail.com>
2024-07-19 12:58:50 +0530

    change

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -16,9 +16,24 @@ import React from 'react';
 import CV from "../../assets/Khushi's resume.pdf";
 
 const CTA = () => {
+  const handleDownloadClick = (event) => {
+    event.preventDefault();
+    // Open the resume in a new tab
+    const newWindow = window.open(CV, '_blank');
+    newWindow.opener = window;
+
+    // Trigger the download
+    const link = document.createElement('a');
+    link.href = CV;
+    link.download = "Khushi's resume.pdf";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+
   return (
     <div className='cta'>
-        <a href={CV} download className='btn' target='_blank' rel="noopener noreferrer">Download CV</a>
+        <a href={CV} className='btn' target='_blank' onClick={handleDownloadClick}>Download CV</a>
         <a href='#contact' className='btn btn-primary'>Let's Talk</a>
     </div>
   );
